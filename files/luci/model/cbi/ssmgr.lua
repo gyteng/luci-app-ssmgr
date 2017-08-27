0e8934e0bedbcab7eb74667fa9f76e2de834dab1
gyteng <igyteng@gmail.com>
2017-08-27 12:30:53 +0800

    bug fix

--- files/luci/model/cbi/ssmgr.lua
+++ files/luci/model/cbi/ssmgr.lua
@@ -1,6 +1,6 @@
 local m, s, o
 
-if luci.sys.call("pidof ssmgr >/dev/null") == 0 then
+if luci.sys.call("pidof ssmgr > /dev/null") == 0 then
 	m = Map("ssmgr", translate("ssmgr"), "%s - %s" %{translate("ssmgr"), translate("RUNNING")})
 else
 	m = Map("ssmgr", translate("ssmgr"), "%s - %s" %{translate("ssmgr"), translate("NOT RUNNING")})
@@ -21,12 +21,13 @@ o.rmempty     = false
 o = s:option(Flag, "alloc", translate("Alloc by server"))
 o.rmempty     = false
 
-local apply = luci.http.formvalue("cbi.apply")
+apply = luci.http.formvalue("cbi.apply")
+
 if apply then
 	site = uci.get("ssmgr", "default", "site")
-	luci.sys.call("echo "..site.." > /usr/share/autoGetAccount/website.txt")
-	luci.sys.call("sh /usr/share/autoGetAccount/cron.sh")
-	luci.sys.call("sh /usr/share/autoGetAccount/start.sh")
+	luci.sys.exec("echo "..site.." > /usr/share/autoGetAccount/website.txt")
+	luci.sys.exec("sh /usr/share/autoGetAccount/cron.sh")
+	luci.sys.exec("sh /usr/share/autoGetAccount/start.sh")
 end
 
 return m
--- files/root/usr/share/autoGetAccount/start.sh
+++ files/root/usr/share/autoGetAccount/start.sh
@@ -19,14 +19,28 @@ then
   echo 'account not change'
 else
   echo $newAccount > $accountFile
-  address=$(cat ${accountFile} | ./JSON.sh -l | egrep '\["address"\]' | awk '{print $2}')
-  port=$(cat ${accountFile} | ./JSON.sh -l | egrep '\["port"\]' | awk '{print $2}')
-  password=$(cat ${accountFile} | ./JSON.sh -l | egrep '\["password"\]' | awk '{print $2}')
-  method=$(cat ${accountFile} | ./JSON.sh -l | egrep '\["method"\]' | awk '{print $2}')
+  address=$(cat ${accountFile} | ./JSON.sh -l | egrep '\["default","address"\]' | awk '{print $2}')
+  port=$(cat ${accountFile} | ./JSON.sh -l | egrep '\["default","port"\]' | awk '{print $2}')
+  password=$(cat ${accountFile} | ./JSON.sh -l | egrep '\["default","password"\]' | awk '{print $2}')
+  method=$(cat ${accountFile} | ./JSON.sh -l | egrep '\["default","method"\]' | awk '{print $2}')
   uci set shadowsocks-libev.@shadowsocks-libev[0].server=${address//\"/}
   uci set shadowsocks-libev.@shadowsocks-libev[0].server_port=${port//\"/}
   uci set shadowsocks-libev.@shadowsocks-libev[0].password=${password//\"/}
   uci set shadowsocks-libev.@shadowsocks-libev[0].encrypt_method=${method//\"/}
   uci commit shadowsocks-libev
+  stop=0
+  i=1
+  uci del ssmgr.@ssmgr[0].server_list
+  while [ $stop -eq 0 ]
+  do
+    server=$(cat ${accountFile} | ./JSON.sh -l | egrep '\["servers",'+$i+',"address"\]' | awk '{print $2}')
+    if [ -z "$server" ]; then
+      stop=1
+    else
+      uci add_list ssmgr.@ssmgr[0].server_list=${server//\"/}
+      let i+=1
+    fi
+  done
+  uci commit ssmgr
   /etc/init.d/shadowsocks-libev restart
 fi
