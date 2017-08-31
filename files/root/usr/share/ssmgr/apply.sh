21e3218459e557d9f99d8f6354adaaa1977731e3
gyteng <igyteng@gmail.com>
2017-08-31 19:36:54 +0800

    bug fix

---
+++ files/root/usr/share/ssmgr/apply.sh
@@ -0,0 +1,2 @@
+cd /usr/share/ssmgr
+./start.sh > /dev/null &
--- files/root/usr/share/ssmgr/getAccount.sh
+++
@@ -1,9 +0,0 @@
-cd /usr/share/ssmgr
-accountFile="./account.txt"
-ssmgrAddress=$(uci get ssmgr.@ssmgr[0].site)
-macAddress=`ifconfig | grep 'eth0' | awk '{print $5}' | sed 's/\://g'`
-newAccount=$(curl -s ${ssmgrAddress}api/user/account/mac/${macAddress})
-if [ ${#newAccount} -lt 10 ]; then
-  return
-fi
-echo $newAccount > $accountFile
